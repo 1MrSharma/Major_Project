b28c646f3d2e75eb38513839fdeab4cae173ca52
1MrSharma <9411732222shubham@gmail.com>
2019-01-21 01:14:38 +0530

    Change

--- Main/Main/File.h
+++ Main/Main/File.h
@@ -1,11 +1,11 @@
-#pragma once //a statement to preprocessor that exexcute statement...A request topreprocessor
+#pragma once 
 #include<Windows.h>
 class CFile
 {
 private:
 	HANDLE m_hFile;
 	LPCTSTR m_szFileName;
-	// Does the 2 attributes written below are really required? Question yourself  
+	
 	DWORD m_cdwDesiredAccess;
 	DWORD m_dwShareMode;
 public:
