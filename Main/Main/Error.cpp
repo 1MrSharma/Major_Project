4e3a61ff42151ac4e8f4e69aa13aee9bce69924b
1MrSharma <9411732222shubham@gmail.com>
2019-01-31 11:05:11 +0530

    31_01_2019#2

--- Main/Main/Error.cpp
+++ Main/Main/Error.cpp
@@ -5,6 +5,11 @@
 TCHAR CError::geterror(void)
 {
 	m_dwErrCode =GetLastError();
+	printerrormessege(m_dwChar);
+}
+
+DWORD CError::printerrormessege(DWORD)
+{
 	m_dwChar = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, m_dwErrCode, 0, m_wszMsgBuffer, 512, NULL);
-	printf("Error value: %d Message: %ws\n" , m_dwErrCode , m_dwChar ? m_wszMsgBuffer : L"Error message not found");
+	printf("Error value: %d Message: %ws\n", m_dwErrCode, m_dwChar ? m_wszMsgBuffer : L"Error message not found");
 }
