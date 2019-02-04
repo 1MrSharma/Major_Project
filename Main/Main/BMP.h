15a870802b6cfe41409e5bff901bc305a4c42bff
1MrSharma <9411732222shubham@gmail.com>
2019-02-04 13:43:56 +0530

    04_02_2019

--- Main/Main/BMP.h
+++ Main/Main/BMP.h
@@ -3,14 +3,14 @@
 class CBMP
 {
 public:
-	DWORD type_file;//00 (2 bytes)
-	DWORD bitmap_width_pixel;//12 (4 bytes)
-	DWORD bitmap_height_pixel;//16 (4 bytes)
-	DWORD number_color_planes;//1A (2 bytes)
-	DWORD bits_per_pixel;//1C (2 bytes)
-	LPCTSTR compression_method;//1E (4 bytes)
-	DWORD horizontal_resolution;//26 (4 bytes)
-	DWORD vertical_resolution;//starting 2A (4 bytes)
-	DWORD color_palette;//starting 2E (4 bytes)
+	DWORD m_dwtype_file;//00 (2 bytes)
+	DWORD m_dwbitmap_width_pixel;//12 (4 bytes)
+	DWORD m_dwbitmap_height_pixel;//16 (4 bytes)
+	DWORD m_dwnumber_color_planes;//1A (2 bytes)
+	DWORD m_dwbits_per_pixel;//1C (2 bytes)
+	LPCTSTR m_szcompression_method;//1E (4 bytes)
+	DWORD m_dwhorizontal_resolution;//26 (4 bytes)
+	DWORD m_dwvertical_resolution;//starting 2A (4 bytes)
+	DWORD m_dwcolor_palette;//starting 2E (4 bytes)
 };
 
