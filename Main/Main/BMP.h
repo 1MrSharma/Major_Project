4139f2f98f01e136b6a73d07bbd335d22f328162
1MrSharma <9411732222shubham@gmail.com>
2019-02-19 12:17:39 +0530

    19_02_2019#2

--- Main/Main/BMP.h
+++ Main/Main/BMP.h
@@ -3,8 +3,22 @@
 class CBMP
 {
 private:
-	INT * pbStructurepointer;
+	BYTE * pbStructurepointer;
 public:
 	void setStructurepointer(BYTE);
+	BOOL checkFiletype(WORD);
+	DWORD returnSizeBMP(DWORD);
+	DWORD returnOffsetpixelarray(DWORD);
+	DWORD returnSizebitmapinfoheader(DWORD);
+	INT32 returnBitamapwidth(INT32);
+	INT32 returnBitmapheight(INT32);
+	WORD returnColorplanes(WORD);
+	WORD returnColordepth(WORD);
+	DWORD returnCompressionmethod();
+	DWORD returnRawimagesize(DWORD);
+	DWORD returnHorizontalresolution(DWORD);
+	DWORD returnVerticalresolution(DWORD);
+	DWORD returnColorpallete(DWORD);
+	DWORD returnImportantcolors(DWORD);
 };
 
